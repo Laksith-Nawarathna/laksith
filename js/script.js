ae621543587cfc852a3b6a1a89d6ea8b9b1ebc82
Eranga Bandara <erangabandara94@gmail.com>
2022-12-06 10:03:41 +0530

    Add animation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings:["Full Stack Developer", "Trainee Software Engineer", "Researcher"],
+    typeSpeed:100,
+    BackSpeed: 75,
+    loop:true
+})
